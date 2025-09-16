b8a50be68acae94e8fc77cb52e0c1179579bf74a
michael rouger <mika.lm049@gmail.com>
2025-09-16 15:52:51 +0200

    chore: require ostruct early to silence warning

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,5 @@
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
+require "ostruct" # Ensure gem version of ostruct is loaded before stdlib fallback
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
